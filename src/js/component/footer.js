d9381e99fc15ba2b58ad2a5858e198342737caa1
maoc86 <soymanuweb@gmail.com>
2023-08-31 03:16:18 +0000

    primero

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <div className="bg-dark text-center text-muted text-white-50">
+      <p className="p-4 m-0">Copyright © MANUWEB 2023</p>
+    </div>
+  </footer>
 );
